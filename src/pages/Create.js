abf9435a6c5dcba6000a57fd9caa322d88e76ef5
Akila Samarawickrama <akilaishere@gmail.com>
2021-07-09 01:58:01 +0530

    Buttons Done

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 
 export default function Create() {
   return (
-    <div>
+    <Container>
       <Typography
         variant='h6'
         component='h2'
@@ -12,6 +14,26 @@ export default function Create() {
       >
         Create a New Note
       </Typography>
-    </div>
+
+      <Button
+        onClick={() => console.log('You Clicked Me')}
+        type='submit'
+        color='secondary'
+        variant='contained'
+      >
+        Submit
+      </Button>
+
+      {/* <Button type='submit'>Submit</Button>
+      <Button type='submit' variant='outlined' color='secondary'>
+        Submit
+      </Button>
+
+      <ButtonGroup color='secondary' variant='contained'>
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+    </Container>
   );
 }
